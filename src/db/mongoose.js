1c853d801afbc72506e0acc8f9359c5e1619798f
Khamtanh <laotrader23@gmail.com>
2025-07-04 13:09:15 +0300

    config cloud mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 export async function connectDB() {
-  const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/weizen';
+  // const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/weizen';
+  const MONGODB_URI = process.env.MONGODB_URI || 
+    'mongodb+srv://laotrader23:ZMtCxf%25VnF5kLWA@cluster0.exebvmy.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0';
+
 
   try {
     await mongoose.connect(MONGODB_URI, {
