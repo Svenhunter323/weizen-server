1f46613b095e1bcd6c201be7797df550c8dba7fd
Khamtanh <laotrader23@gmail.com>
2025-07-30 22:05:12 +0300

    mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 export async function connectDB() {
-  // const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/weizen';
-  const MONGODB_URI = process.env.MONGODB_URI || 
-    'mongodb://localhost:27017/weizen';
+  const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://laotrader23:ZMtCxf%25VnF5kLWA@cluster0.exebvmy.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0';
+  // const MONGODB_URI = process.env.MONGODB_URI || 
+  //   'mongodb://localhost:27017/weizen';
 
 
   try {
